5ded23bbfe622da06f106866079e20cf676059c1
verixx <abdurraqeeb53@gmail.com>
2017-10-21 19:18:47 +1100

    Added a docstring and a clear option for presence

--- bot.py
+++ bot.py
@@ -378,6 +378,9 @@ class Modmail(commands.Bot):
     @commands.command(name="customstatus", aliases=['status', 'presence'])
     @commands.has_permissions(administrator=True)
     async def _status(self, ctx, *, message):
+        '''Set a custom playing status for the bot.'''
+        if message == 'clear':
+            return await self.change_presence(game=None)
         await self.change_presence(game=discord.Game(name=message), status=discord.Status.online)
         await ctx.send(f"Changed status to **{message}**")
 
