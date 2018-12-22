d715504befcf8e85c75beec1f13baeafbc6f6466
verixx <abdurraqeeb53@gmail.com>
2018-12-22 23:41:39 +1100

    Increment version

--- bot.py
+++ bot.py
@@ -199,7 +199,7 @@ class Modmail(commands.Bot):
         em.add_field(name='Custom Mentions', value=mention)
         em.add_field(name='Warning', value=warn)
         em.add_field(name='Github', value='https://github.com/kyb3r/modmail')
-        em.set_footer(text='Modmail v1.1.2 | Star the repository to unlock hidden features! /s')
+        em.set_footer(text='Modmail v1.1.3 | Star the repository to unlock hidden features! /s')
 
         return em
 
