115c52cd5b46ed2bfdf5c0cf54092889dc625983
verixx <abdurraqeeb53@gmail.com>
2018-12-23 18:08:08 +1100

    Added an update command

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 
 import discord
 from discord.ext import commands
@@ -41,6 +41,38 @@ import traceback
 import io
 import inspect
 from contextlib import redirect_stdout
+
+class Github:
+    head = 'https://api.github.com/repos/kyb3r/modmail/git/refs/heads/master'
+    merge_url = 'https://api.github.com/repos/{username}/modmail/merges'
+
+    def __init__(self, bot, access_token, username=None):
+        self.bot = bot
+        self.session = bot.session
+        self.access_token = access_token
+        self.username = username
+        self.headers = {'Authorization': 'Bearer '+access_token}
+    
+    async def update_repository(self):
+        sha = (await self.request(self.head))['object']['sha']
+        payload = {
+            'base': 'master',
+            'head': sha,
+            'commit_message': 'Updating bot'
+        }
+        merge_url = self.merge_url.format(username=self.username)
+
+        await self.request(merge_url, method='POST', payload=payload)
+
+    async def request(self, url, method='GET', payload=None):
+        async with self.session.request(method, url, headers=self.headers, json=payload) as resp:
+            return await resp.json()
+    
+    @classmethod
+    async def login(cls, bot, access_token):
+        self = cls(bot, access_token)
+        self.username = (await self.request('https://api.github.com/user'))['login']
+        return self
 
 
 class Modmail(commands.Bot):
@@ -284,6 +316,37 @@ class Modmail(commands.Bot):
 
         em.set_footer(text=footer)
 
+        await ctx.send(embed=em)
+    
+    
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def update(self, ctx):
+        await ctx.trigger_typing()
+
+        async with self.session.get('https://api.kybr.tk/modmail') as resp:
+            data = await resp.json()
+        
+        em = discord.Embed(
+                title='Cannot update',
+                description=f'The bot is already up to date v`{__version__}`',
+                color=discord.Color.green()
+            )
+
+        if data['latest_version'] == __version__:
+            return await ctx.send(embed=em)
+
+        access_token = self.config.get('GITHUB_ACCESS_TOKEN')
+
+        if not access_token:
+            em.description = 'You have not properly set up github credentials.'
+            em.color = discord.Color.red()
+            return await ctx.send(embed=em)
+        
+        user = await Github.login(self, access_token)
+        await user.update_repository()
+        em.title = 'Success'
+        em.description = 'Bot successfully updated, the bot will restart momentarily'
         await ctx.send(embed=em)
 
     @commands.command()
