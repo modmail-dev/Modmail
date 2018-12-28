45177e2fbad67f9b24afa891912e0b67aa08d2a8
verixx <abdurraqeeb53@gmail.com>
2018-12-28 17:47:03 +1100

    Increment version

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-__version__ = '1.3.7'
+__version__ = '1.3.8'
 
 from contextlib import redirect_stdout
 from urllib.parse import urlparse
