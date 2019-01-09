39b5a5a3ee48f582d30b1b30b327bf0343e73acb
kyb3r <abdurraqeeb53@gmail.com>
2019-01-09 15:44:52 +1100

    Fix emoji handling

--- bot.py
+++ bot.py
@@ -29,12 +29,14 @@ import textwrap
 import datetime
 import os
 import re
+from types import SimpleNamespace
 
 import discord
 import aiohttp
 from discord.ext import commands
 from discord.ext.commands.view import StringView
 from colorama import init, Fore, Style
+import emoji
 
 from core.api import Github, ModmailApiClient
 from core.thread import ThreadManager
@@ -182,11 +184,28 @@ class ModmailBot(commands.Bot):
         else:
             await self.threads.populate_cache()
 
+
+
     async def process_modmail(self, message):
         """Processes messages sent to the bot."""
 
+        ctx = SimpleNamespace(bot=self, guild=self.modmail_guild)
+        converter = commands.EmojiConverter()
+
         blocked_emoji = self.config.get('blocked_emoji', '🚫')
         sent_emoji = self.config.get('sent_emoji', '✅')
+
+        if blocked_emoji not in emoji.UNICODE_EMOJI:
+            try:
+                blocked_emoji = await converter.convert(ctx, blocked_emoji.strip(':'))
+            except:
+                pass
+
+        if sent_emoji not in emoji.UNICODE_EMOJI:
+            try:
+                sent_emoji = await converter.convert(ctx, sent_emoji.strip(':'))
+            except:
+                pass
 
         reaction = blocked_emoji if message.author.id in self.blocked_users else sent_emoji
 
