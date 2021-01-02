528dcdbcfca681a76c45fc293425c584cd83595d
fourjr <28086837+fourjr@users.noreply.github.com>
2021-01-02 23:29:58 +0800

    Linting

--- core/thread.py
+++ core/thread.py
@@ -660,7 +660,7 @@ class Thread:
     async def find_linked_message_from_dm(self, message, either_direction=False):
         if either_direction and message.embeds:
             compare_url = message.embeds[0].author.url
-            compare_id = compare_url.split('#')[-1]
+            compare_id = compare_url.split("#")[-1]
         else:
             compare_url = None
             compare_id = None
@@ -683,7 +683,7 @@ class Thread:
                     return linked_message
 
                 if compare_id is not None and compare_id.isdigit():
-                    if int(msg_id) == int (compare_id):
+                    if int(msg_id) == int(compare_id):
                         return linked_message
 
             raise ValueError("Thread channel message not found.")
