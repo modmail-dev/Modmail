c1241d5ea6c46b57716f125d25a2078eb34fd1ae
Chris Lovering <chris.lovering.95@gmail.com>
2023-06-19 09:18:20 +0100

    Escape hyphen in regex string
    Unescaped this was permitting any character between $ (index 36) and _ (index 95), which aren't all valid in urls.

--- core/thread.py
+++ core/thread.py
@@ -995,7 +995,7 @@ class Thread:
                 attachments.append(attachment)
 
         image_urls = re.findall(
-            r"http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+            r"http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$\-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
             message.content,
         )
 
