a4e27c5d2bc2a5cd3d765cd6dde8a7e07ec61907
lorenzo132 <lhoorn4@gmail.com>
2026-08-02 13:43:05 +0200

    Preserve forwarded attachments with snippets

--- core/thread.py
+++ core/thread.py
@@ -2028,6 +2028,14 @@ class Thread:
             else:
                 attachments.append(attachment)
 
+        # Forwarded attachments are represented only in ``ext`` rather than
+        # ``message.attachments``, so classify the remaining entries separately.
+        for attachment in ext[len(message.attachments) :]:
+            if is_image_url(attachment[0]):
+                images.append(attachment)
+            else:
+                attachments.append(attachment)
+
         image_urls = re.findall(
             r"http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
             message.content,
